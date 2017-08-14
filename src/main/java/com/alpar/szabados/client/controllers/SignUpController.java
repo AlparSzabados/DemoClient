be5f319d48cb06fed9eda5a59f7f18ef3b94e654
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-14 11:55:23 +0300

    Modified

--- src/main/java/com/alpar/szabados/client/controllers/SignUpController.java
+++ src/main/java/com/alpar/szabados/client/controllers/SignUpController.java
@@ -1,28 +1,54 @@
 package com.alpar.szabados.client.controllers;
 
-import com.alpar.szabados.client.connectors.UserBean;
-import com.alpar.szabados.client.entities.User;
+import com.alpar.szabados.client.beans.UserBean;
+import com.alpar.szabados.client.utils.SessionUtils;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Component
 @SessionScoped
 @ManagedBean(name = "signUpController")
-@Join(path = "/signup", to = "/signup.jsf")
+@Join(path = "/sign-up", to = "/sign-up.jsf")
 public class SignUpController {
-	private User user = new User();
+	private long userId;
+	private String userName;
+	private String password;
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private UserBean userBean = new UserBean();
 
 	public String create() throws IOException {
 		UserBean userBean = new UserBean();
-		userBean.createUser(user.getUserName(), user.getPassword());
-		return "/hello.xhtml?faces-redirect=true";
-	}
-
-	public User getUser() {
-		return user;
+		HttpSession session = SessionUtils.getSession();
+		session.setAttribute("username", userName);
+		if (userBean.createUser(userName, password)) {
+			return "/add-activity.xhtml?faces-redirect=true";
+		} else {
+			return "/sign-up.xhtml?faces-redirect=true";
+		}
 	}
 }
