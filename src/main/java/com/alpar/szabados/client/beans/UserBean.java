9b2ca59f895ca75e5f62fe7ed9646b5ee804038d
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-14 11:25:26 +0300

    Simplified `UserBean`

---
+++ src/main/java/com/alpar/szabados/client/beans/UserBean.java
@@ -0,0 +1,25 @@
+package com.alpar.szabados.client.beans;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import java.io.IOException;
+
+public class UserBean {
+	private static final String SERVER_PATH = "http://localhost:8090/user/";
+
+	public boolean createUser(String userName, String password) throws IOException {
+		String path = SERVER_PATH + "createUser/" + userName + "." + password;
+		WebResource webResource = Client.create().resource(path);
+		ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
+		return response.getStatus() == 200;
+	}
+
+	public boolean validateUser(String userName, String password) throws IOException {
+		String path = SERVER_PATH + "validateUser/" + userName + "." + password;
+		WebResource webResource = Client.create().resource(path);
+		ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
+		return response.getStatus() == 200;
+	}
+}
