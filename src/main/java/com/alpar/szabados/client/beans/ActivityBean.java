f1677085e4dbb134d23aa406d0749f94ba27f017
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-16 21:17:00 +0300

    Unified response validation

--- src/main/java/com/alpar/szabados/client/beans/ActivityBean.java
+++ src/main/java/com/alpar/szabados/client/beans/ActivityBean.java
@@ -7,49 +7,55 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
+import org.springframework.stereotype.Component;
 
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 import static com.alpar.szabados.client.entities.TaskStatus.NOT_COMPLETED;
 import static org.codehaus.jackson.map.SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS;
 
 // TODO make it work for spaces also
+@Component
 public class ActivityBean {
-	// Path to REST Service URI
-	private static final String SERVER_PATH = "http://localhost:8090/activity/";
+    // Path to REST Service URI
+    private static final String HOST = System.getProperty("server.host", "localhost");
+    private static final String PORT = System.getProperty("server.port", "8090");
+    private static final String SERVER_PATH = "http://" + HOST + ":" + PORT + "/activity/"; // TODO should work from other host also
 
-	public Activity[] findUserActivities(String userName) throws IOException {
-		String path = SERVER_PATH + "findActivities/" + userName;
-		WebResource webResource = Client.create().resource(path);
-		ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
-		if (response.getStatus() == 200) {
-			ObjectMapper mapper = new ObjectMapper();
-			JSONArray myObject = response.getEntity(JSONArray.class);
-			mapper.configure(FAIL_ON_EMPTY_BEANS, false);
-			//TODO refactor
-			return mapper.readValue(myObject.toString(), Activity[].class);
-		}
-		return new Activity[0];
-	}
+    public Activity[] findUserActivities(String userName) throws IOException {
+        String path = SERVER_PATH + "findActivities/" + userName;
+        WebResource webResource = Client.create().resource(path); // TODO relative path?
+        ClientResponse response = webResource.type("application/json").get(ClientResponse.class); // TODO change to POST from GET
+        if (isOk(response)) {
+            ObjectMapper mapper = new ObjectMapper().configure(FAIL_ON_EMPTY_BEANS, false);
 
-	public boolean createActivity(String activityName, String userName) throws IOException {
-		String path = SERVER_PATH + "createActivity/" + activityName + "." + NOT_COMPLETED + "." + userName;
-		WebResource webResource = Client.create().resource(path);
-		ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
-		return response.getStatus() == 200;
-	}
+            //TODO refactor
+            JSONArray myObject = response.getEntity(JSONArray.class);
+            return mapper.readValue(myObject.toString(), Activity[].class);
+        }
+        return new Activity[0];
+    }
 
+    public boolean createActivity(String activityName, String userName) throws IOException { // TODO rename
+        String path = SERVER_PATH + "createActivity/" + activityName + "." + NOT_COMPLETED + "." + userName;
+        WebResource webResource = Client.create().resource(path);
+        ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
+        return isOk(response);
+    }
 
-	public boolean completeTask(String[] selectedActivities) {
-		int responseStatus = 0;
-		for (String selectedActivity : selectedActivities) {
-			String path = SERVER_PATH + "completeTask/" + selectedActivity + "." + SessionUtils.getUserName();
-			WebResource webResource = Client.create().resource(path);
-			ClientResponse response = webResource.type("application/json").post(ClientResponse.class);
-			responseStatus = response.getStatus();
-		}
-		return responseStatus == 200;
-	}
+    public boolean completeTask(String[] selectedActivities) {
+        boolean result = true;
+        for (String selectedActivity : selectedActivities) {
+            String path = SERVER_PATH + "completeTask/" + selectedActivity + "." + SessionUtils.getUserName();
+            WebResource webResource = Client.create().resource(path);
+            ClientResponse response = webResource.type("application/json").post(ClientResponse.class);
+            result = result && isOk(response);
+        }
+        return result;
+    }
+
+    public static boolean isOk(ClientResponse response) { // TODO move to helper
+        return response.getStatus() == Response.Status.OK.getStatusCode();
+    }
 }
-
-
--- src/main/java/com/alpar/szabados/client/beans/UserBean.java
+++ src/main/java/com/alpar/szabados/client/beans/UserBean.java
@@ -3,24 +3,28 @@ package com.alpar.szabados.client.beans;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static com.alpar.szabados.client.beans.ActivityBean.isOk;
+
 // TODO make it work for spaces also
+@Component
 public class UserBean {
-	private static final String SERVER_PATH = "http://localhost:8090/user/";
+    private static final String SERVER_PATH = "http://localhost:8090/user/";
 
-	public boolean createUser(String userName, String password) throws IOException {
-		String path = SERVER_PATH + "createUser/" + userName + "." + password;
-		WebResource webResource = Client.create().resource(path);
-		ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
-		return response.getStatus() == 200;
-	}
+    public boolean createUser(String userName, String password) throws IOException {
+        String path = SERVER_PATH + "createUser/" + userName + "." + password;
+        WebResource webResource = Client.create().resource(path);
+        ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
+        return isOk(response);
+    }
 
-	public boolean validateUser(String userName, String password) throws IOException {
-		String path = SERVER_PATH + "validateUser/" + userName + "." + password;
-		WebResource webResource = Client.create().resource(path);
-		ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
-		return response.getStatus() == 200;
-	}
+    public boolean validateUser(String userName, String password) throws IOException {
+        String path = SERVER_PATH + "validateUser/" + userName + "." + password;
+        WebResource webResource = Client.create().resource(path);
+        ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
+        return isOk(response);
+    }
 }
