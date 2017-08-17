38b8c9e7e79084b37daee9051aff35cbf6c5bf2b
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-17 20:49:09 +0300

    Simplified the code, removed boilerplate

--- src/main/java/com/alpar/szabados/client/controllers/LoginController.java
+++ src/main/java/com/alpar/szabados/client/controllers/LoginController.java
@@ -1,54 +1,41 @@
 package com.alpar.szabados.client.controllers;
 
 import com.alpar.szabados.client.beans.UserBean;
+import com.alpar.szabados.client.entities.User;
 import com.alpar.szabados.client.utils.SessionUtils;
 import org.ocpsoft.rewrite.annotation.Join;
-import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@Component
-@SessionScoped
 @ManagedBean(name = "loginController")
 @Join(path = "/", to = "/login.jsf")
 public class LoginController {
-    private String userName;
-    private String password;
+    private User user = new User();
     private UserBean userBean = new UserBean();
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String validateUsernamePassword() throws IOException {
-        if (userBean.validateUser(userName, password)) {
+        if (userBean.validateUser(user.getUserName(), user.getPassword())) {
             HttpSession session = SessionUtils.getSession();
-            session.setAttribute("username", userName);
+            session.setAttribute("username", user.getUserName());
             return "/add-activity.xhtml?faces-redirect=true";
         } else {
             return "/login.xhtml?faces-redirect=true";
         }
     }
 
-    //TODO use it
     public String logout() {
         HttpSession session = SessionUtils.getSession();
         session.invalidate();
         return "/login.xhtml?faces-redirect=true";
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/com/alpar/szabados/client/controllers/SignUpController.java
+++ src/main/java/com/alpar/szabados/client/controllers/SignUpController.java
@@ -1,53 +1,35 @@
 package com.alpar.szabados.client.controllers;
 
 import com.alpar.szabados.client.beans.UserBean;
+import com.alpar.szabados.client.entities.User;
 import com.alpar.szabados.client.utils.SessionUtils;
 import org.ocpsoft.rewrite.annotation.Join;
-import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@Component
-@SessionScoped
 @ManagedBean(name = "signUpController")
 @Join(path = "/sign-up", to = "/sign-up.jsf")
 public class SignUpController {
-    private long userId;
-    private String userName;
-    private String password;
+    private User user = new User();
     private UserBean userBean = new UserBean();
 
-    public long getUserId() {
-        return userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String create() throws IOException {
-        UserBean userBean = new UserBean();
         HttpSession session = SessionUtils.getSession();
-        session.setAttribute("username", userName);
-        if (userBean.createUser(userName, password)) {
+        session.setAttribute("username", user.getUserName());
+        if (userBean.createUser(user.getUserName(), user.getPassword())) {
             return "/add-activity.xhtml?faces-redirect=true";
         } else {
             return "/sign-up.xhtml?faces-redirect=true";
         }
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
