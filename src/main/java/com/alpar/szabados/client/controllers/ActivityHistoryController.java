c310ab83bcfce0070875d98532d276d98c96027f
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:50:53 +0300

    Minor refactoring

--- src/main/java/com/alpar/szabados/client/controllers/ActivityHistoryController.java
+++ src/main/java/com/alpar/szabados/client/controllers/ActivityHistoryController.java
@@ -2,15 +2,11 @@ package com.alpar.szabados.client.controllers;
 
 import com.alpar.szabados.client.beans.ActivityBean;
 import com.alpar.szabados.client.entities.Activity;
+import com.alpar.szabados.client.entities.User;
 import com.alpar.szabados.client.utils.SessionUtils;
 import org.ocpsoft.rewrite.annotation.Join;
-import org.ocpsoft.rewrite.annotation.RequestAction;
-import org.ocpsoft.rewrite.faces.annotation.Deferred;
-import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
-import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +15,7 @@ import java.util.List;
 @Join(path = "/history", to = "/activity-history.jsf")
 public class ActivityHistoryController {
     private ActivityBean activityBean = new ActivityBean();
-    private List<Activity> activities = Arrays.asList(activityBean.findUserActivities(SessionUtils.getUserName()));
+    private List<Activity> activities = Arrays.asList(activityBean.findUserActivities(new User(SessionUtils.getUserName())));
 
     public ActivityHistoryController() throws IOException {
     }
