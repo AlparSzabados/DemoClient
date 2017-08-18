ec01e96617b96bed3d0212d27a8481309677593b
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 19:06:42 +0300

    Added wrapper class to be used for un-marshaling JSON

---
+++ src/main/java/com/alpar/szabados/client/entities/UserAndActivityWrapper.java
@@ -0,0 +1,34 @@
+package com.alpar.szabados.client.entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class UserAndActivityWrapper implements Serializable {
+    private User user;
+    private Activity activity;
+
+    public UserAndActivityWrapper() {
+    }
+
+    public UserAndActivityWrapper(User user, Activity activity) {
+        this.user = user;
+        this.activity = activity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+}
