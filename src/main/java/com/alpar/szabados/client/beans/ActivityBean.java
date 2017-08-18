2fcd51a891b58cd6e2228b3a92940f4ec5fe66ca
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:41:21 +0300

    Merged common content. Methods now send JSON objects instead of path parameters

--- src/main/java/com/alpar/szabados/client/beans/ActivityBean.java
+++ src/main/java/com/alpar/szabados/client/beans/ActivityBean.java
@@ -1,54 +1,57 @@
 package com.alpar.szabados.client.beans;
 
 import com.alpar.szabados.client.entities.Activity;
-import com.alpar.szabados.client.utils.SessionUtils;
+import com.alpar.szabados.client.entities.User;
+import com.alpar.szabados.client.entities.UserAndActivityWrapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jettison.json.JSONArray;
-import org.springframework.stereotype.Component;
 
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.Arrays;
 
+import static com.alpar.szabados.client.entities.TaskStatus.COMPLETED;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.codehaus.jackson.map.SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS;
 
-// TODO make it work for spaces also
 public class ActivityBean {
     private static final String HOST = System.getProperty("server.host", "localhost");
     private static final String PORT = System.getProperty("server.port", "8090");
     private static final String SERVER_PATH = "http://" + HOST + ":" + PORT + "/activity/"; // TODO should work from other host also
 
-    public Activity[] findUserActivities(String userName) throws IOException {
-        String path = SERVER_PATH + "findActivities/" + userName;
+    public Activity[] findUserActivities(User user) throws IOException {
+        String path = SERVER_PATH + "findActivities/";
         WebResource webResource = Client.create().resource(path); // TODO relative path?
-        ClientResponse response = webResource.type("application/json").get(ClientResponse.class); // TODO change to POST from GET
+        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, user); // TODO change to POST from GET
         if (isOk(response)) {
             ObjectMapper mapper = new ObjectMapper().configure(FAIL_ON_EMPTY_BEANS, false);
-
-            JSONArray myObject = response.getEntity(JSONArray.class);
-            return mapper.readValue(myObject.toString(), Activity[].class);
+            return mapper.readValue(response.getEntityInputStream(), Activity[].class);
+        } else {
+            return new Activity[0]; // TODO huh?
         }
-        return new Activity[0];
     }
 
-    public boolean createActivity(String activityName, String userName) throws IOException {
-        String path = SERVER_PATH + "createActivity/" + activityName + "." + userName;
+    public boolean completeTask(String[] selectedActivities, User user) throws IOException {
+        return Arrays.stream(selectedActivities)
+                     .map(selectedActivity -> new Activity(selectedActivity, COMPLETED))
+                     .allMatch(activity -> createActivity(user, activity));
+    }
+
+    public boolean createActivity(User user, Activity activity) {
+        UserAndActivityWrapper wrapper = new UserAndActivityWrapper(user, activity);
+        String path = SERVER_PATH + "createActivity/";
         WebResource webResource = Client.create().resource(path);
-        ClientResponse response = webResource.type("application/json").put(ClientResponse.class);
-        return isOk(response);
-    }
-
-    public boolean completeTask(String[] selectedActivities) {
-        boolean result = true;
-        for (String selectedActivity : selectedActivities) {
-            String path = SERVER_PATH + "completeTask/" + selectedActivity + "." + SessionUtils.getUserName();
-            WebResource webResource = Client.create().resource(path);
-            ClientResponse response = webResource.type("application/json").post(ClientResponse.class);
-            result = result && isOk(response);
+        ObjectMapper mapper = new ObjectMapper().configure(FAIL_ON_EMPTY_BEANS, false);
+        String valueAsString = "";
+        try {
+            valueAsString = mapper.writeValueAsString(wrapper);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return result;
+        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, valueAsString);
+        return isOk(response);
     }
 
     public static boolean isOk(ClientResponse response) { // TODO move to helper
