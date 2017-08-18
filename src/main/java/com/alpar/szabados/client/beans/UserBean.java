27cbc7d2d5303d8f19bcb61180dbc57c3f66c2e8
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:42:13 +0300

    Removed redundant code

--- src/main/java/com/alpar/szabados/client/beans/UserBean.java
+++ src/main/java/com/alpar/szabados/client/beans/UserBean.java
@@ -4,11 +4,11 @@ import com.alpar.szabados.client.entities.User;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 
 import static com.alpar.szabados.client.beans.ActivityBean.isOk;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class UserBean {
     private static final String SERVER_PATH = "http://localhost:8090/user/";
@@ -16,16 +16,14 @@ public class UserBean {
     public boolean createUser(User user) throws IOException {
         String path = SERVER_PATH + "createUser/";
         WebResource webResource = Client.create().resource(path);
-        ClientResponse response = webResource.type("application/json").put(ClientResponse.class, user);
+        ClientResponse response = webResource.type(APPLICATION_JSON).put(ClientResponse.class, user);
         return isOk(response);
     }
 
     public boolean validateUser(User user) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonInString = mapper.writeValueAsString(user);
         String path = SERVER_PATH + "validateUser/";
         WebResource webResource = Client.create().resource(path);
-        ClientResponse response = webResource.type("application/json").post(ClientResponse.class, jsonInString);
+        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, user);
         return isOk(response);
     }
 }
