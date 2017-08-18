277327e770c9efb740b0636e66f3804b9f396319
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:53:42 +0300

    Updated according to the ActivityBean modifications

--- src/main/java/com/alpar/szabados/client/controllers/AddActivityController.java
+++ src/main/java/com/alpar/szabados/client/controllers/AddActivityController.java
@@ -1,13 +1,15 @@
 package com.alpar.szabados.client.controllers;
 
 import com.alpar.szabados.client.beans.ActivityBean;
+import com.alpar.szabados.client.entities.Activity;
+import com.alpar.szabados.client.entities.User;
 import com.alpar.szabados.client.utils.SessionUtils;
 import org.ocpsoft.rewrite.annotation.Join;
-import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import java.io.IOException;
+
+import static com.alpar.szabados.client.entities.TaskStatus.NOT_COMPLETED;
 
 @ManagedBean(name = "addActivity")
 @Join(path = "/add-activity", to = "/add-activity.jsf")
@@ -16,7 +18,7 @@ public class AddActivityController {
     private ActivityBean activityBean = new ActivityBean();
 
     public String createActivity() throws IOException {
-        activityBean.createActivity(activityName, SessionUtils.getUserName());
+        activityBean.createActivity(new User(SessionUtils.getUserName()), new Activity(activityName, NOT_COMPLETED));
         return "/activities.xhtml?faces-redirect=true";
     }
 
