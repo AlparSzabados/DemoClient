961a23557367a3c293e2673156370f5c82f5d670
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-20 15:50:06 +0300

    Added empty field verification

--- src/main/java/com/alpar/szabados/client/controllers/SignUpController.java
+++ src/main/java/com/alpar/szabados/client/controllers/SignUpController.java
@@ -2,11 +2,11 @@ package com.alpar.szabados.client.controllers;
 
 import com.alpar.szabados.client.beans.UserBean;
 import com.alpar.szabados.client.entities.User;
+import com.alpar.szabados.client.handlers.MessageFactory;
 import com.sun.jersey.api.client.ClientResponse;
 import org.ocpsoft.rewrite.annotation.Join;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import java.io.IOException;
 
 import static com.alpar.szabados.client.handlers.ResponseHandler.handleResponse;
@@ -20,10 +20,15 @@ public class SignUpController {
     private UserBean userBean = new UserBean();
 
     public String create() throws IOException {
+        if (user.getUserName().isEmpty() || user.getPassword().isEmpty()) {
+            MessageFactory.info("FIELD CAN'T BE EMPTY");
+            return "";
+        }
+
         ClientResponse response = userBean.createUser(this.user);
         if (isOk(response)) {
             getSession().setAttribute("username", this.user.getUserName());
-            return "add-activity.xhtml";
+            return "activities.xhtml";
         }
         return handleResponse(response, null);
     }
